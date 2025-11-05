fe281aecc7e45b1b9eb1bd3c9b79e25381bdb4c9
Pop&Go Experiences <popandgoexperiences@gmail.com>
2025-11-05 23:02:01 +0000

    solamente tengo un menú que no funciona

--- src/components/sidebar-nav.tsx
+++ src/components/sidebar-nav.tsx
@@ -58,15 +58,16 @@ export function SidebarNav() {
         <SidebarMenu>
           {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-              <Link href={item.href} passHref>
-                <SidebarMenuButton
-                  isActive={pathname === item.href}
-                  tooltip={{ children: item.label, side: 'right' }}
-                >
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === item.href}
+                tooltip={{ children: item.label, side: 'right' }}
+              >
+                <Link href={item.href}>
                   <item.icon />
                   <span>{item.label}</span>
-                </SidebarMenuButton>
-              </Link>
+                </Link>
+              </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
         </SidebarMenu>
