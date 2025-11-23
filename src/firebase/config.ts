b9e15db95fd41b6d35baf65810083d6dfa521548
Pop&Go Experiences <popandgoexperiences@gmail.com>
2025-11-23 16:38:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5273799396-24b72",
+  "appId": "1:1033236341814:web:03679de0e0bc91a86a2497",
+  "apiKey": "AIzaSyB2tf6SPFQqrjJHeRMmCiunlIiAaCizgYk",
+  "authDomain": "studio-5273799396-24b72.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1033236341814"
+};
