05172008efc86c315db47dfb1aa1f02d190c76b6
Pop&Go Experiences <popandgoexperiences@gmail.com>
2026-01-18 17:28:57 +0000

    Cambia el botón del desplegable del menú en smartphone hacia el lado izq

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,8 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 export function Header() {
   return (
     <header className="sticky top-0 z-10 flex h-14 items-center justify-between gap-4 border-b bg-background px-4 md:hidden">
-      <Logo className="h-6" />
       <SidebarTrigger />
+      <Logo className="h-6" />
     </header>
   );
 }
